a60334920c43e06d607b6087cabb2e8873cd9acb
Dmitriy <dimalie@live.com>
2021-04-30 19:36:52 +0300

    Practise 2

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'Use strict';
 
 
-
-const filmCount = prompt('Сколько фильмов уже посмотрели?', '');
+const filmCount = +prompt('Сколько фильмов уже посмотрели?', '');
 
 const personalMovieDB = {
     count: filmCount,
@@ -12,13 +11,56 @@ const personalMovieDB = {
     privat: false,
 };
 
+if ( filmCount <= 10){
+    const message = alert('Просмотренно довольно мало фильмов');
+} else if (filmCount >= 10 && filmCount.length <= 30) {
+    const message = alert('Просмотренно достаточно фильмов');
+} else if (filmCount > 30) {
+    const message = alert('Вы киноман!');
+} else {
+    const message = alert('Нужно ввести число');
+}
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB.movies);
+
+
+// for (let i = 0 ; i < 2; i++) {
+    
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько его оцените?', '');
+//           if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length <50) {
+//             personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }
+// }
+
+
+// console.log(personalMovieDB);
+
+// let count = 0;
+
+// while ( count < 2 ) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько его оцените?', '');
+    
+//     if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length <50) {
+//         personalMovieDB.movies[a] = b;
+//         count++;
+// } 
+
+// }
+
+// console.log(personalMovieDB);
+
+// let count = 0;
+
+// do {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//      b = prompt('На сколько его оцените?', '');
+//      if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length <50) {
+//              personalMovieDB.movies[a] = b;
+//              count++;
+//      } 
+// } while ( count < 2 ) ;
