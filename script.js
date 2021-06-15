b239359e5a4a7a61b2ef9ff52e8a763c415eb87a
Dmitriy <dimalie@live.com>
2021-06-15 11:40:58 +0300

    Promise 055

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
-'use strict';
-class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }                                       //концепция
+// 'use strict';
 
-    calcArea() {
-        return this.height * this.width;
-    }
-}
+// console.log('Запрос данных....');
 
-class ColoredRectangleWithText extends Rectangle{
-    constructor(height, width, text, bgColor) {
-        super(height, width);
-        this.text = text;
-        this.bgColor = bgColor;
-    }
+// const req = new Promise((resolve, reject) => {
 
-    showMyProps() {
-        console.log(`Текст: ${this.text}, цвет:${this.bgColor}`);
-    }
-}
+//     setTimeout(() => {
+//         console.log('Подготовка данных....');
+    
+//         const product = {
+//             name: 'TV',
+//             price: 2000,
+//         };
+        
+//         resolve(product);
+//     }, 2000);
 
-const div = new ColoredRectangleWithText(25, 10, 'hello', 'red');
+// });
 
-div.showMyProps();
-console.log(div.calcArea());
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 product.status = 'order';
+//                 resolve(product);
+//             }, 2000); 
+//     });
+// }).then(product => { // аргмент продукт может иметь любое название, но он отображает приняттую информацию от прошлого шага.
+//     product.modify = true;
+//     return product;
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('Ошибка'); 
+// }).finally(() => {
+//     console.log('finally');
+    
+// });
 
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    });
+};
 
-const square = new Rectangle(10, 10);      //экземпляры
-const long = new Rectangle(10, 32);
+// test(1000).then(() => console.log('1000ms'));
+// test(2000).then(() => console.log('2000ms'));
 
-console.log(square.calcArea());
-console.log(long.calcArea());
+// Promise.all([test(1000), test(2000)]).then(() => {
+    // console.log('All'); 
+// });
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('All');
+});
